63b3fc8f4b48f50083b7f96738f0ae2eee65013e
Payal Kumavat <payalkumavat02@gmail.com>
2025-06-24 16:44:47 +0530

    Add streaming support for LLM response with flush for real-time output

--- app.py
+++ app.py
@@ -22,17 +22,27 @@ def get_horoscope(sign: str, day: str = 'TODAY'):
 user_input = input("Enter your horoscope sign: ").strip()
 
 horoscope_text = get_horoscope(user_input)
-# with open('text_file.txt', 'r') as f:
-#     text = f.read().strip()
-model='llama3'
-prompt = f"""You're a fortune teller who tells horoscopes of different signs.
+
+message = [
+    {
+        'role': 'user',
+        'content': f"""You're a fortune teller who tells horoscopes of different signs.
 The horoscope of the user is {horoscope_text} tell that horoscope to the user in a mom way. That is
 be sarcastic, kind and you have to tell the horoscopes in grade 5 level english. Use
 short sentences, be pessimistic, rude, judgemental too. Dont write everything
 related to horoscope sign. Make fun of the user. 
 Don't write the sign name while telling the horoscope. Don't write more than 5-10 lines.
 """
+    }
+]
 
-response = ollama.generate(model=model, prompt=prompt)
-generated_horoscope = response.get('response', '')
-print(generated_horoscope)
+stream = chat(
+    model='llama3',
+    messages=message,
+    stream=True,
+)
+for chunk in stream:
+    print(chunk['message']['content'],end='',flush=True)
+# response = ollama.generate(model=model, prompt=prompt)
+# generated_horoscope = response.get('response', '')
+# print(generated_horoscope)
