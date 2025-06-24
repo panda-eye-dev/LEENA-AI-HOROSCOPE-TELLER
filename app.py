8fccc9b06b17a3ff857596ddb19af68c6e929d2b
Payal Kumavat <payalkumavat02@gmail.com>
2025-06-24 12:52:09 +0530

    Add 3rd party horoscope api to make the response more dynamic and real-time

--- app.py
+++ app.py
@@ -3,16 +3,30 @@ import ollama
 from ollama import chat
 from ollama import ChatResponse
 
-text_file = 'text_file.txt'
-user_input = input("Enter your horoscope sign: ")
+def get_horoscope(sign: str, day: str = 'TODAY'):
+    base_url = 'https://horoscope-app-api.vercel.app/get-horoscope/daily?'
+    params = {
+        'sign': sign.capitalize(),
+        'day': day.upper()
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        return data.get('horoscope_data', 'No horoscope found')
+    except Exception as e:
+        return f"Error fetching horoscope: {e}"
+    
+user_input = input("Enter your horoscope sign: ").strip()
+
+horoscope_text = get_horoscope(user_input)
+# with open('text_file.txt', 'r') as f:
+#     text = f.read().strip()
 model='llama3'
-
-with open('text_file.txt', 'r') as f:
-    text = f.read().strip()
-
 prompt = f"""You're a fortune teller who tells horoscopes of different signs.
-Please see the {user_input} for the sign that the user has inputted. Then look for that
-signs horoscope in {text} and tell that horoscope to the user in a mom way. That is
+The horoscope of the user is {horoscope_text} tell that horoscope to the user in a mom way. That is
 be sarcastic, kind and you have to tell the horoscopes in grade 5 level english. Use
 short sentences, be pessimistic, rude, judgemental too. Dont write everything
 related to horoscope sign. Make fun of the user. 
